87d32e837e34df7adebb4806db354e9584dc8714
Neel Tangella <ntangella@eit.org>
2025-01-08 19:40:48 +0000

    more import debugging yay

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
       name='functional_eval',
       version='0.0.1',
       description='functional evaluation pipeline for SALT workflow',
-      packages=['physiochemical', 'docking', 'immunogenicity', 'docking.utils', 'immunogenicity.utils', 'immunogenicity.utils.bp3'],
+      packages=['physiochemical', 'docking', 'immunogenicity', 'docking.utils', 'immunogenicity.utils', 'immunogenicity.utils.bp3', 'immunogenicity.utils.bp3.BP3Models'],
       install_requires=[
             'tqdm',
             'numpy',
