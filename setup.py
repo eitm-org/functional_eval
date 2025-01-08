6de33b16c9b359844996639630f2509c7a1dc13a
Neel Tangella <ntangella@eit.org>
2025-01-08 20:05:45 +0000

    more

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ setup(
       name='functional_eval',
       version='0.0.1',
       description='functional evaluation pipeline for SALT workflow',
-      packages=['physiochemical', 'docking', 'immunogenicity', 'docking.utils', 'immunogenicity.utils', 'immunogenicity.utils.bp3', 'immunogenicity.utils.bp3.BP3Models'],
+      packages=['physiochemical', 'docking', 'immunogenicity', 'docking.utils', 'immunogenicity.utils', 'immunogenicity.utils.bp3', 
+                'immunogenicity.utils.bp3.BP3Models', 'immunogenicity.utils.bp3.BP3Models.BP3C50IDFFNN', 'immunogenicity.utils.bp3.BP3Models.BP3C50IDSeqLenFFNN'],
       install_requires=[
             'tqdm',
             'numpy',
