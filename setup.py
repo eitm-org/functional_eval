d2b0d10e0623a6ede3ae9a3ba671af6745c83a31
Neel Tangella <ntangella@eit.org>
2025-01-22 17:30:05 +0000

    modified setup.py to remove bp3 models since it gets created

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(
       name='functional_eval',
       version='0.0.1',
       description='functional evaluation pipeline for SALT workflow',
-      packages=['physiochemical', 'docking', 'immunogenicity', 'docking.utils', 'immunogenicity.utils', 'immunogenicity.utils.bp3', 
-                'immunogenicity.utils.bp3.BP3Models', 'immunogenicity.utils.bp3.BP3Models.BP3C50IDFFNN', 'immunogenicity.utils.bp3.BP3Models.BP3C50IDSeqLenFFNN'],
+      packages=['physiochemical', 'docking', 'immunogenicity', 'docking.utils', 'immunogenicity.utils', 'immunogenicity.utils.bp3'],
       install_requires=[
             'tqdm',
             'numpy',
